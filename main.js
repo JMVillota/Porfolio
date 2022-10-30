9974b0acff548ecdd34eb40ed3ee8693cfcfbf57
JMVillota <jeffersonvillota@gmail.com>
2022-10-30 11:10:19 -0500

    Se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,5 @@
+import "./style.css";
+import Experience from "./Experience/Experience.js";
+
+// get our fontawesome imports
+const experience = new Experience(document.querySelector(".experience-canvas"));
